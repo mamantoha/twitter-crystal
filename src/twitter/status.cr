0351ff8cffe1dfee0c48acf96179f540d6dce6f4
Anton Maminov <anton.linux@gmail.com>
2020-02-26 15:48:59 +0200

    change User object

--- src/twitter/status.cr
+++ src/twitter/status.cr
@@ -2,11 +2,32 @@ require "json"
 
 module Twitter
   class Status
-    JSON.mapping(
-      id: Int64,
-      id_str: String,
-      user_id: Int64,
-      user_id_str: String,
-    )
+    include JSON::Serializable
+
+    property created_at : String
+
+    property favorite_count : Int32?
+
+    property favorited : Bool
+
+    property id : Float64
+
+    property in_reply_to_screen_name : String?
+
+    property in_reply_to_status_id : Float64?
+
+    property in_reply_to_user_id : Float64?
+
+    property lang : String?
+
+    property retweet_count : Int32
+
+    property retweeted : Bool
+
+    property source : String
+
+    property text : String
+
+    property truncated : Bool
   end
 end
--- src/twitter/user.cr
+++ src/twitter/user.cr
@@ -2,50 +2,50 @@ require "json"
 
 module Twitter
   class User
-    JSON.mapping({
-      contributors_enabled:               Bool,
-      created_at:                         {type: Time, converter: Time::Format.new("%a %b %d %T +0000 %Y")},
-      default_profile:                    Bool,
-      default_profile_image:              Bool,
-      favourites_count:                   Int32,
-      follow_request_sent:                Bool?,
-      followers_count:                    Int32,
-      following:                          Bool?,
-      friends_count:                      Int32,
-      geo_enabled:                        Bool,
-      id:                                 Int64,
-      is_translation_enabled:             Bool?,
-      is_translator:                      Bool,
-      lang:                               String?,
-      listed_count:                       Int32,
-      location:                           String?,
-      name:                               String,
-      needs_phone_verification:           Bool?,
-      notifications:                      Bool?,
-      profile_background_color:           String,
-      profile_background_image_url:       String?,
-      profile_background_image_url_https: String?,
-      profile_background_tile:            Bool,
-      profile_banner_url:                 String?,
-      profile_image_url:                  String?,
-      profile_image_url_https:            String?,
-      profile_link_color:                 String,
-      profile_location:                   String | Location | Nil,
-      profile_sidebar_border_color:       String,
-      profile_sidebar_fill_color:         String,
-      profile_text_color:                 String,
-      profile_use_background_image:       Bool,
-      protected:                          Bool,
-      screen_name:                        String,
-      status:                             Twitter::Tweet?,
-      statuses_count:                     Int32,
-      suspended:                          Bool?,
-      time_zone:                          String?,
-      url:                                String?,
-      utc_offset:                         Int32?,
-      verified:                           Bool,
-      description:                        String?,
-    })
+    include JSON::Serializable
+
+    @[JSON::Field(converter: Time::Format.new("%a %b %d %T +0000 %Y"))]
+    property created_at : Time
+
+    property default_profile : Bool
+
+    property default_profile_image : Bool
+
+    property favourites_count : Int32
+
+    property followers_count : Int32
+
+    property friends_count : Int32
+
+    property id : Int64
+
+    property listed_count : Int32
+
+    property location : String
+
+    property name : String
+
+    property needs_phone_verification : Bool?
+
+    property profile_banner_url : String?
+
+    property profile_image_url_https : String
+
+    @[JSON::Field(key: "protected")]
+    property user_protected : Bool
+
+    property screen_name : String
+
+    property status : Status?
+
+    property statuses_count : Int32
+
+    property suspended : Bool?
+
+    property verified : Bool
+
+    property description : String
+
     def_equals id
   end
 end
