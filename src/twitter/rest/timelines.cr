e5461509d26450f88f8ec7af1183e4365b384bc9
Anton Maminov <anton.linux@gmail.com>
2020-02-27 01:13:22 +0200

    remove redundant comments

--- src/twitter/rest/timelines.cr
+++ src/twitter/rest/timelines.cr
@@ -1,10 +1,6 @@
 module Twitter
   module REST
     module Timelines
-      # perform_get_with_objects('/1.1/statuses/mentions_timeline.json', options, Twitter::Tweet)
-      # objects_from_response_with_user(Twitter::Tweet, :get, '/1.1/statuses/user_timeline.json', args)
-      # perform_get_with_objects('/1.1/statuses/home_timeline.json', options, Twitter::Tweet)
-      # perform_get_with_objects('/1.1/statuses/retweets_of_me.json', options, Twitter::Tweet)
       def home_timeline(options = {} of String => String) : Array(Twitter::Tweet)
         response = get("/1.1/statuses/home_timeline.json", options)
         Array(Twitter::Tweet).from_json(response)
