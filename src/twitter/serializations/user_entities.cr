6aa3c1c5aec3e555d5666c325f26279b1a4d2d9d
Anton Maminov <anton.linux@gmail.com>
2020-02-27 00:49:38 +0200

    add user entities

---
+++ src/twitter/serializations/user_entities.cr
@@ -0,0 +1,11 @@
+require "json"
+
+module Twitter
+  class UserEntities
+    include JSON::Serializable
+
+    property description : UserDescriptionEntity?
+
+    property url : UserUrlEntity?
+  end
+end
---
+++ src/twitter/serializations/user_url_entity.cr
@@ -0,0 +1,9 @@
+require "json"
+
+module Twitter
+  class UserUrlEntity
+    include JSON::Serializable
+
+    property urls : Array(UrlEntity)?
+  end
+end
